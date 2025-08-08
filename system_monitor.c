d420000a7bf12a0bdff72017913887b4545c8849
syazwansaidan93 <78026725+syazwansaidan93@users.noreply.github.com>
2025-08-08 17:11:40 +0800

    Update system_monitor.c

--- system_monitor.c
+++ system_monitor.c
@@ -6,14 +6,11 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <dirent.h>
 #include <pthread.h>
 #include <time.h>
-#include <sys/utsname.h>
 
 #define PORT 3040
 #define BUFFER_SIZE 4096
-#define MAX_PATH 256
 #define THREAD_POOL_SIZE 4
 
 typedef struct {
@@ -26,14 +23,18 @@ typedef struct {
     pthread_cond_t not_empty;
 } thread_pool_queue;
 
+typedef struct {
+    double cpu_usage_percent;
+    double net_upload_bytes_sec;
+    double net_download_bytes_sec;
+    unsigned long long net_total_bytes_sent;
+    unsigned long long net_total_bytes_recv;
+} live_stats_t;
+
 thread_pool_queue client_queue;
 pthread_mutex_t stats_mutex;
-
-static unsigned long long last_cpu_total = 0;
-static unsigned long long last_cpu_busy = 0;
-static unsigned long long last_net_bytes_sent = 0;
-static unsigned long long last_net_bytes_recv = 0;
-static time_t last_net_timestamp = 0;
+pthread_mutex_t live_stats_mutex;
+live_stats_t live_stats;
 
 void init_queue(int size) {
     client_queue.queue = (int*)malloc(sizeof(int) * size);
@@ -70,91 +71,40 @@ unsigned long long get_uptime_seconds() {
     FILE *fp;
     double uptime_seconds;
     fp = fopen("/proc/uptime", "r");
-    if (fp == NULL) {
-        return 0;
-    }
+    if (!fp) return 0;
     fscanf(fp, "%lf", &uptime_seconds);
     fclose(fp);
     return (unsigned long long)uptime_seconds;
 }
 
-double get_cpu_usage_percent() {
-    pthread_mutex_lock(&stats_mutex);
-    FILE *fp = fopen("/proc/stat", "r");
-    if (fp == NULL) {
-        pthread_mutex_unlock(&stats_mutex);
-        return 0.0;
-    }
-    unsigned long long user, nice, system, idle;
-    char line[256];
-    fgets(line, sizeof(line), fp);
-    sscanf(line, "cpu %llu %llu %llu %llu", &user, &nice, &system, &idle);
-    fclose(fp);
-    unsigned long long busy_time = user + nice + system;
-    unsigned long long total_time = busy_time + idle;
-    double cpu_percent = 0.0;
-    if (last_cpu_total != 0) {
-        unsigned long long delta_total = total_time - last_cpu_total;
-        unsigned long long delta_busy = busy_time - last_cpu_busy;
-        if (delta_total > 0) {
-            cpu_percent = ((double)delta_busy / (double)delta_total) * 100.0;
-        }
-    }
-    last_cpu_total = total_time;
-    last_cpu_busy = busy_time;
-    pthread_mutex_unlock(&stats_mutex);
-    return cpu_percent;
-}
-
 void get_ram_swap_stats(unsigned long long *ram_total_kb, unsigned long long *ram_used_kb,
                         unsigned long long *swap_total_kb, unsigned long long *swap_used_kb) {
     FILE *fp = fopen("/proc/meminfo", "r");
-    if (fp == NULL) {
-        *ram_total_kb = 0;
-        *ram_used_kb = 0;
-        *swap_total_kb = 0;
-        *swap_used_kb = 0;
+    if (!fp) {
+        *ram_total_kb = *ram_used_kb = *swap_total_kb = *swap_used_kb = 0;
         return;
     }
     unsigned long long total_mem = 0, free_mem = 0, buffers = 0, cached = 0;
     unsigned long long total_swap = 0, free_swap = 0;
     char line[256];
     while (fgets(line, sizeof(line), fp)) {
-        if (strstr(line, "MemTotal:") == line) {
-            sscanf(line, "MemTotal: %llu kB", &total_mem);
-        } else if (strstr(line, "MemFree:") == line) {
-            sscanf(line, "MemFree: %llu kB", &free_mem);
-        } else if (strstr(line, "Buffers:") == line) {
-            sscanf(line, "Buffers: %llu kB", &buffers);
-        } else if (strstr(line, "Cached:") == line) {
-            sscanf(line, "Cached: %llu kB", &cached);
-        } else if (strstr(line, "SwapTotal:") == line) {
-            sscanf(line, "SwapTotal: %llu kB", &total_swap);
-        } else if (strstr(line, "SwapFree:") == line) {
-            sscanf(line, "SwapFree: %llu kB", &free_swap);
-        }
+        if (strstr(line, "MemTotal:") == line) sscanf(line, "MemTotal: %llu kB", &total_mem);
+        else if (strstr(line, "MemFree:") == line) sscanf(line, "MemFree: %llu kB", &free_mem);
+        else if (strstr(line, "Buffers:") == line) sscanf(line, "Buffers: %llu kB", &buffers);
+        else if (strstr(line, "Cached:") == line) sscanf(line, "Cached: %llu kB", &cached);
+        else if (strstr(line, "SwapTotal:") == line) sscanf(line, "SwapTotal: %llu kB", &total_swap);
+        else if (strstr(line, "SwapFree:") == line) sscanf(line, "SwapFree: %llu kB", &free_swap);
     }
     fclose(fp);
     *ram_total_kb = total_mem;
     *swap_total_kb = total_swap;
-    if (total_mem > 0) {
-        *ram_used_kb = total_mem - free_mem - buffers - cached;
-    } else {
-        *ram_used_kb = 0;
-    }
-    if (total_swap > 0) {
-        *swap_used_kb = total_swap - free_swap;
-    } else {
-        *swap_used_kb = 0;
-    }
+    *ram_used_kb = (total_mem > 0) ? total_mem - free_mem - buffers - cached : 0;
+    *swap_used_kb = (total_swap > 0) ? total_swap - free_swap : 0;
 }
 
 int get_cpu_temp_millicelsius() {
-    FILE *fp;
-    fp = fopen("/sys/class/thermal/thermal_zone2/temp", "r");
-    if (fp == NULL) {
-        return 0;
-    }
+    FILE *fp = fopen("/sys/class/thermal/thermal_zone2/temp", "r");
+    if (!fp) return 0;
     int temp_millicelcius;
     fscanf(fp, "%d", &temp_millicelcius);
     fclose(fp);
@@ -164,8 +114,7 @@ int get_cpu_temp_millicelsius() {
 void get_disk_usage(const char *path, unsigned long long *total_bytes, unsigned long long *used_bytes, double *percent) {
     struct statvfs vfs;
     if (statvfs(path, &vfs) != 0) {
-        *total_bytes = 0;
-        *used_bytes = 0;
+        *total_bytes = *used_bytes = 0;
         *percent = 0.0;
         return;
     }
@@ -174,52 +123,99 @@ void get_disk_usage(const char *path, unsigned long long *total_bytes, unsigned 
     unsigned long long used = total - free;
     *total_bytes = total;
     *used_bytes = used;
-    if (total > 0) {
-        *percent = (double)used / (double)total * 100.0;
-    } else {
-        *percent = 0.0;
-    }
+    *percent = (total > 0) ? (double)used / (double)total * 100.0 : 0.0;
 }
 
-void get_network_stats(double *upload_speed_bytes_sec, double *download_speed_bytes_sec, unsigned long long *total_bytes_sent, unsigned long long *total_bytes_recv) {
-    pthread_mutex_lock(&stats_mutex);
-    FILE *fp = fopen("/proc/net/dev", "r");
-    if (fp == NULL) {
-        pthread_mutex_unlock(&stats_mutex);
-        *upload_speed_bytes_sec = 0.0;
-        *download_speed_bytes_sec = 0.0;
-        *total_bytes_sent = 0;
-        *total_bytes_recv = 0;
-        return;
-    }
-    unsigned long long current_sent = 0, current_recv = 0;
-    char line[512];
-    while (fgets(line, sizeof(line), fp)) {
-        char *iface = strstr(line, ":");
-        if (iface) {
-            unsigned long long recv_bytes, sent_bytes;
-            sscanf(iface, ":%llu %*u %*u %*u %*u %*u %*u %*u %llu", &recv_bytes, &sent_bytes);
-            current_recv += recv_bytes;
-            current_sent += sent_bytes;
+double get_cpu_usage_percent() {
+    pthread_mutex_lock(&live_stats_mutex);
+    double val = live_stats.cpu_usage_percent;
+    pthread_mutex_unlock(&live_stats_mutex);
+    return val;
+}
+
+void get_network_stats(double *upload_speed_bytes_sec,
+                       double *download_speed_bytes_sec,
+                       unsigned long long *total_bytes_sent,
+                       unsigned long long *total_bytes_recv) {
+    pthread_mutex_lock(&live_stats_mutex);
+    *upload_speed_bytes_sec = live_stats.net_upload_bytes_sec;
+    *download_speed_bytes_sec = live_stats.net_download_bytes_sec;
+    *total_bytes_sent = live_stats.net_total_bytes_sent;
+    *total_bytes_recv = live_stats.net_total_bytes_recv;
+    pthread_mutex_unlock(&live_stats_mutex);
+}
+
+void* stats_updater(void* arg) {
+    unsigned long long last_cpu_total = 0, last_cpu_busy = 0;
+    unsigned long long last_net_sent = 0, last_net_recv = 0;
+    time_t last_time = time(NULL);
+    while (1) {
+        FILE *fp = fopen("/proc/stat", "r");
+        if (fp) {
+            char line[1024];
+            if (fgets(line, sizeof(line), fp)) {
+                unsigned long long vals[16] = {0};
+                int n = 0;
+                char *saveptr = NULL;
+                strtok_r(line, " \t\n", &saveptr);
+                char *tok;
+                while ((tok = strtok_r(NULL, " \t\n", &saveptr)) && n < 16) {
+                    vals[n++] = strtoull(tok, NULL, 10);
+                }
+                unsigned long long total = 0;
+                for (int i = 0; i < n; i++) total += vals[i];
+                unsigned long long idle_all = 0;
+                if (n > 3) idle_all += vals[3];
+                if (n > 4) idle_all += vals[4];
+                unsigned long long busy = total - idle_all;
+                if (last_cpu_total != 0) {
+                    unsigned long long delta_total = total - last_cpu_total;
+                    unsigned long long delta_busy = busy - last_cpu_busy;
+                    if (delta_total > 0) {
+                        pthread_mutex_lock(&live_stats_mutex);
+                        live_stats.cpu_usage_percent = ((double)delta_busy / (double)delta_total) * 100.0;
+                        pthread_mutex_unlock(&live_stats_mutex);
+                    }
+                }
+                last_cpu_total = total;
+                last_cpu_busy = busy;
+            }
+            fclose(fp);
         }
-    }
-    fclose(fp);
-    *upload_speed_bytes_sec = 0.0;
-    *download_speed_bytes_sec = 0.0;
-    time_t current_timestamp = time(NULL);
-    if (last_net_timestamp != 0) {
-        double delta_time = difftime(current_timestamp, last_net_timestamp);
-        if (delta_time > 0) {
-            *upload_speed_bytes_sec = (double)(current_sent - last_net_bytes_sent) / delta_time;
-            *download_speed_bytes_sec = (double)(current_recv - last_net_bytes_recv) / delta_time;
+        fp = fopen("/proc/net/dev", "r");
+        if (fp) {
+            char line[512];
+            unsigned long long sent_total = 0, recv_total = 0;
+            fgets(line, sizeof(line), fp);
+            fgets(line, sizeof(line), fp);
+            while (fgets(line, sizeof(line), fp)) {
+                char *iface = strstr(line, ":");
+                if (iface) {
+                    unsigned long long recv_bytes, sent_bytes;
+                    sscanf(iface, ":%llu %*u %*u %*u %*u %*u %*u %*u %llu",
+                           &recv_bytes, &sent_bytes);
+                    recv_total += recv_bytes;
+                    sent_total += sent_bytes;
+                }
+            }
+            fclose(fp);
+            time_t now = time(NULL);
+            double delta_t = difftime(now, last_time);
+            if (last_time != 0 && delta_t > 0) {
+                pthread_mutex_lock(&live_stats_mutex);
+                live_stats.net_upload_bytes_sec = (double)(sent_total - last_net_sent) / delta_t;
+                live_stats.net_download_bytes_sec = (double)(recv_total - last_net_recv) / delta_t;
+                live_stats.net_total_bytes_sent = sent_total;
+                live_stats.net_total_bytes_recv = recv_total;
+                pthread_mutex_unlock(&live_stats_mutex);
+            }
+            last_net_sent = sent_total;
+            last_net_recv = recv_total;
+            last_time = now;
         }
+        sleep(1);
     }
-    *total_bytes_sent = current_sent;
-    *total_bytes_recv = current_recv;
-    last_net_bytes_sent = current_sent;
-    last_net_bytes_recv = current_recv;
-    last_net_timestamp = current_timestamp;
-    pthread_mutex_unlock(&stats_mutex);
+    return NULL;
 }
 
 void process_request(int client_socket) {
@@ -295,13 +291,15 @@ int main() {
     int addrlen = sizeof(address);
     int opt = 1;
     pthread_t threads[THREAD_POOL_SIZE];
+    pthread_t updater_thread;
     pthread_mutex_init(&stats_mutex, NULL);
+    pthread_mutex_init(&live_stats_mutex, NULL);
+    memset(&live_stats, 0, sizeof(live_stats));
     init_queue(100);
-    
     for (int i = 0; i < THREAD_POOL_SIZE; i++) {
         pthread_create(&threads[i], NULL, worker_thread_function, NULL);
     }
-    
+    pthread_create(&updater_thread, NULL, stats_updater, NULL);
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("socket failed");
         exit(EXIT_FAILURE);
@@ -321,16 +319,15 @@ int main() {
         perror("listen");
         exit(EXIT_FAILURE);
     }
-    
     printf("Starting system monitor API on port %d with a thread pool of %d workers\n", PORT, THREAD_POOL_SIZE);
-    while(1) {
+    while (1) {
         if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
             perror("accept");
             exit(EXIT_FAILURE);
         }
         enqueue(new_socket);
     }
-    
     pthread_mutex_destroy(&stats_mutex);
+    pthread_mutex_destroy(&live_stats_mutex);
     return 0;
 }
